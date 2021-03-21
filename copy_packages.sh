289ccb578df50a87b0cdfa244d9e75ad869767d4
Fredrik Karlsson <fredrik@fldc.se>
2021-03-21 17:49:04 +0100

    change dirx

--- copy_packages.sh
+++ copy_packages.sh
@@ -2,5 +2,5 @@
 
 ssh fredrik@192.168.1.4 rm -rf /opt/docker-data/nginx/www/buffy.fldc.se/public/openwrt/redmi-ac2100/mt7621/packages
 ssh fredrik@192.168.1.4 rm -rf /opt/docker-data/nginx/www/buffy.fldc.se/public/openwrt/redmi-ac2100/mipsel_24kc/
-scp -r /home/fredrik/src/openwrt/bin/targets/ramips/mt7621/packages fredrik@192.168.1.4:/opt/docker-data/nginx/www/buffy.fldc.se/public/openwrt/redmi-ac2100/mt7621
-scp -r /home/fredrik/src/openwrt/bin/packages/mipsel_24kc fredrik@192.168.1.4:/opt/docker-data/nginx/www/buffy.fldc.se/public/openwrt/redmi-ac2100/
+scp -r /home/fredrik/src/openwrt-ac2100/bin/targets/ramips/mt7621/packages fredrik@192.168.1.4:/opt/docker-data/nginx/www/buffy.fldc.se/public/openwrt/redmi-ac2100/mt7621
+scp -r /home/fredrik/src/openwrt-ac2100/bin/packages/mipsel_24kc fredrik@192.168.1.4:/opt/docker-data/nginx/www/buffy.fldc.se/public/openwrt/redmi-ac2100/
